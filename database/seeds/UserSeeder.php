417f52b13e8e2d331109680969be497f68cf9d2e
arsaga-ikeda <n.ikeda@arsaga.jp>
2020-09-07 20:46:37 +0900

    Add UserSeeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => '田中ユウキ',
+            'email' => 'tanaka@example.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi' // password
+        ]);
+        factory(\App\User::class, 10)->create();
+    }
+}
