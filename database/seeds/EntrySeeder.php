4def47480542965b06719bf0afddff4be7c13e55
Naohito Ikeda <ikepo.japon@gmail.com>
2020-12-31 12:42:45 +0900

    EntrySeederのハードコードを修正

--- database/seeds/EntrySeeder.php
+++ database/seeds/EntrySeeder.php
@@ -15,40 +15,28 @@ class EntrySeeder extends Seeder
      */
     public function run()
     {
-        if(env('APP_ENV') === 'local') {
-            for ($i = 1; $i <= 65; $i++) {
-                DB::table('entries')->insert([
-                    'event_id' => rand(1, 9),
-                    'user_id' => $i,
-                    'paid' => rand(0, 1),
-                    'cancellation_request' => rand(0, 1),
-                    'payment_method' => rand(1, 2),
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                    ]);
-                }
-        } elseif (env('APP_ENV') === 'production') {
-            $events = [];
-            foreach (Event::all() as $event) {
-                $events[] = $event->id;
-            }
+        // 全イベントIDを配列にいれる
+        $events = [];
+        foreach (Event::all() as $event) {
+            $events[] = $event->id;
+        }
 
-            $users = [];
-            foreach (User::all() as $user) {
-                $users[] = $user->id;
-            }
+        // 全ユーザーIDを配列にいれる
+        $users = [];
+        foreach (User::all() as $user) {
+            $users[] = $user->id;
+        }
 
-            for ($i = 1; $i <= 65; $i++) {
-                DB::table('entries')->insert([
-                    'event_id' => $events[array_rand($events, 1)],
-                    'user_id' => $users[array_rand($users, 1)],
-                    'paid' => rand(0, 1),
-                    'cancellation_request' => rand(0, 1),
-                    'payment_method' => rand(1, 2),
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ]);
-            }
+        for ($i = 1; $i <= 65; $i++) {
+            DB::table('entries')->insert([
+                'event_id' => $events[array_rand($events, 1)],
+                'user_id' => $users[array_rand($users, 1)],
+                'paid' => rand(0, 1),
+                'cancellation_request' => rand(0, 1),
+                'payment_method' => rand(1, 2),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
         }
     }
 }
