46dc5e9f43e228eeec1fa49c52a02d01316455e5
Naohito Ikeda <ikepo.japon@gmail.com>
2020-12-31 12:14:08 +0900

    fix EntrySeeder

--- database/seeds/EntrySeeder.php
+++ database/seeds/EntrySeeder.php
@@ -15,16 +15,37 @@ class EntrySeeder extends Seeder
      */
     public function run()
     {
-        for ($i = 1; $i <= 65; $i++) {
-            DB::table('entries')->insert([
-                'event_id' => rand(1, 9),
-                'user_id' => $i,
-                'paid' => rand(0, 1),
-                'cancellation_request' => rand(0, 1),
-                'payment_method' => rand(1, 2),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
+        
+        if(env('APP_ENV') === 'local') {
+            for ($i = 1; $i <= 65; $i++) {
+                DB::table('entries')->insert([
+                    'event_id' => rand(1, 9),
+                    'user_id' => $i,
+                    'paid' => rand(0, 1),
+                    'cancellation_request' => rand(0, 1),
+                    'payment_method' => rand(1, 2),
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                    ]);
+                }
+        } elseif (env('APP_ENV') === 'production') {
+            $events = Event::all();
+            $array = [];
+            foreach($events as $event) {
+                $array[] = $event->id;
+            }
+
+            for ($i = 1; $i <= 65; $i++) {
+                DB::table('entries')->insert([
+                    'event_id' => array_rand($array, 1),
+                    'user_id' => $i,
+                    'paid' => rand(0, 1),
+                    'cancellation_request' => rand(0, 1),
+                    'payment_method' => rand(1, 2),
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            }
         }
     }
 }
