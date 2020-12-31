d78bf5af89c8e65f0d10527fca106d2c32d41a33
Naohito Ikeda <ikepo.japon@gmail.com>
2020-12-31 12:40:07 +0900

    fix EntrySeeder

--- database/seeds/EntrySeeder.php
+++ database/seeds/EntrySeeder.php
@@ -28,13 +28,20 @@ class EntrySeeder extends Seeder
                     ]);
                 }
         } elseif (env('APP_ENV') === 'production') {
-            $events = [1, 11, 21, 31, 41, 51, 61, 71, 81];
-            $users = [1, 11, 21, 31, 41, 51, 61, 71, 81, 91, 101, 111, 121, 131, 141, 151, 161, 171, 181, 191, 201, 211, 221, 231, 241, 251, 261, 271, 281, 291, 301];
-            
+            $events = [];
+            foreach (Event::all() as $event) {
+                $events[] = $event->id;
+            }
+
+            $users = [];
+            foreach (User::all() as $user) {
+                $users[] = $user->id;
+            }
+
             for ($i = 1; $i <= 65; $i++) {
                 DB::table('entries')->insert([
-                    'event_id' => array_rand_value($events, 1),
-                    'user_id' => array_rand_value($users, 1),
+                    'event_id' => $events[array_rand($events, 1)],
+                    'user_id' => $users[array_rand($users, 1)],
                     'paid' => rand(0, 1),
                     'cancellation_request' => rand(0, 1),
                     'payment_method' => rand(1, 2),
@@ -42,20 +49,6 @@ class EntrySeeder extends Seeder
                     'updated_at' => now(),
                 ]);
             }
-        }
-
-        // 配列から指定した要素数分、ランダムに要素を取り出す
-        function array_rand_value(&$input_array, $get_num = 1)
-        {
-            $return_array = array();
-            
-            $key_rand = array_rand($input_array, $get_num);
-            foreach ($key_rand as $key)
-            {
-                $return_array[] = $input_array[$key];
-            }
-            
-            return $return_array;
         }
     }
 }
